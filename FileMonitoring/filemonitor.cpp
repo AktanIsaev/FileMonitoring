a7e9a3f15f93e1c2df0206006c0aa550b3448b2c
Aktan Isaev <aktanisaev350@gmail.com>
2024-04-30 21:44:25 +0700

    Updating the implementation of class FileMonitor

--- FileMonitoring/filemonitor.cpp
+++ FileMonitoring/filemonitor.cpp
@@ -25,6 +25,24 @@ bool FileMonitor::DelFile(QString path)//удаление файла
 
 void FileMonitor::update()//обновление информации о файлах
 {
-
+    for(int i = 0; i < objects.size(); i++)
+    {
+        FileStatus temp = objects[i];
+        if (objects[i].update())
+        {
+            if (temp.GetIsExisting() && !objects[i].GetIsExisting())
+            {
+                emit NotExist(objects[i].GetFilePath());
+            }
+            else if (!temp.GetIsExisting() && objects[i].GetIsExisting())
+            {
+                emit Exist(objects[i].GetFilePath(), objects[i].GetSize());
+            }
+            else
+            {
+                emit Resized(objects[i].GetFilePath(), objects[i].GetSize());
+            }
+        }
+    }
 }
 
